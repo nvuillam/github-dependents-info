0ae13ec974b2a51095678efb94f3feddfa42d6f0
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2022-11-13 03:29:30 +0100

    sort

--- github_dependents_info/gh_dependents_info.py
+++ github_dependents_info/gh_dependents_info.py
@@ -116,25 +116,17 @@ class GithubDependentsInfo:
             "private_dependents_number": self.total_private_sum,
         }
 
-    def build_markdown(self,**options) -> str:
-        md_lines = [
-            f"# Dependents stats for {self.repo}",
-            ""
-            ]
+    def build_markdown(self, **options) -> str:
+        md_lines = [f"# Dependents stats for {self.repo}", ""]
         for dep_repo in self.dependent_repos:
-            md_lines += [
-                "## Package "+dep_repo["name"],
-                ""
-            ]
+            md_lines += ["## Package " + dep_repo["name"], ""]
             for repo1 in dep_repo["public_dependents"]:
-                md_lines += [
-                    "  - "+repo1
-                ]
+                md_lines += ["  - " + repo1]
             md_lines += [""]
         md_lines_str = "\n".join(md_lines)
         if "file" in options:
             with open(options["file"], "w", encoding="utf-8") as f:
                 f.write(md_lines_str)
                 if self.debug:
-                    print("Wrote markdown file "+options["file"])
+                    print("Wrote markdown file " + options["file"])
         return md_lines_str
--- tests/test_gh_dependents_info/test_gh_dependents_info.py
+++ tests/test_gh_dependents_info/test_gh_dependents_info.py
@@ -1,6 +1,7 @@
 """Tests for gh_dependents_info"""
-import os,uuid
+import os
 import tempfile
+import uuid
 
 from github_dependents_info import GithubDependentsInfo
 
@@ -10,17 +11,12 @@ def test_collect_stats_single_package():
     gh_deps_info = GithubDependentsInfo(repo)
     repo_stats = gh_deps_info.collect(debug=True)
     assert repo_stats["public_dependents_number"] > 0
-    tmp_md_file = (
-                tempfile.gettempdir()
-                + os.path.sep
-                + str(uuid.uuid4())
-                + "-test-single.md"
-            )
+    tmp_md_file = tempfile.gettempdir() + os.path.sep + str(uuid.uuid4()) + "-test-single.md"
     md = gh_deps_info.build_markdown(file=tmp_md_file)
-    assert md.count('\n') > 10
+    assert md.count("\n") > 10
     with open(tmp_md_file, "r", encoding="utf-8") as file:
         md_content = file.read()
-        assert md_content.count('\n') > 10
+        assert md_content.count("\n") > 10
 
 
 def test_collect_stats_multi_package():
@@ -28,14 +24,9 @@ def test_collect_stats_multi_package():
     gh_deps_info = GithubDependentsInfo(repo)
     repo_stats = gh_deps_info.collect(debug=True)
     assert repo_stats["public_dependents_number"] > 0
-    tmp_md_file = (
-                tempfile.gettempdir()
-                + os.path.sep
-                + str(uuid.uuid4())
-                + "-test-multiple.md"
-            )
+    tmp_md_file = tempfile.gettempdir() + os.path.sep + str(uuid.uuid4()) + "-test-multiple.md"
     md = gh_deps_info.build_markdown(file=tmp_md_file)
-    assert md.count('\n') > 100
+    assert md.count("\n") > 100
     with open(tmp_md_file, "r", encoding="utf-8") as file:
         md_content = file.read()
-        assert md_content.count('\n') > 100
+        assert md_content.count("\n") > 100
