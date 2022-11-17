d5a8c4d6d744a0aff6fe356a0b05004d399ff952
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2022-11-17 13:11:41 +0100

    Fix cases when dependents count is superior to 1000
    Fixes https://github.com/nvuillam/github-dependents-info/issues/27

--- github_dependents_info/__init__.py
+++ github_dependents_info/__init__.py
@@ -4,10 +4,7 @@ import sys
 
 from .gh_dependents_info import GithubDependentsInfo
 
-if sys.version_info >= (3, 8):
-    from importlib import metadata as importlib_metadata
-else:
-    import importlib_metadata
+from importlib import metadata as importlib_metadata
 
 
 def get_version() -> str:
