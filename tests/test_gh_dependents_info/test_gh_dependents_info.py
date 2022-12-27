04188a6e3dbf5d4c38b53e0a753e2f23b3c7341c
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2022-12-28 00:11:09 +0100

    Update test class

--- tests/test_gh_dependents_info/test_gh_dependents_info.py
+++ tests/test_gh_dependents_info/test_gh_dependents_info.py
@@ -8,12 +8,13 @@ from github_dependents_info import GithubDependentsInfo
 
 def test_collect_stats_single_package():
     repo = "nvuillam/npm-groovy-lint"
-    gh_deps_info = GithubDependentsInfo(repo, debug=True, sort_key="stars")
+    gh_deps_info = GithubDependentsInfo(repo, debug=True, sort_key="stars", badge_color="pink")
     repo_stats = gh_deps_info.collect()
     assert repo_stats["public_dependents_number"] > 10
     tmp_md_file = tempfile.gettempdir() + os.path.sep + str(uuid.uuid4()) + "-test-single.md"
     md = gh_deps_info.build_markdown(file=tmp_md_file)
     assert md.count("\n") > 10
+    assert "pink" in md
     with open(tmp_md_file, "r", encoding="utf-8") as file:
         md_content = file.read()
         assert md_content.count("\n") > 10
